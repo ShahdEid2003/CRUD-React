932c62bb915ccde10da6e3da003cdb083c6877a4
ShahdEid2003 <shahdeid012@gmail.com>
2025-01-14 00:19:59 +0200

    update

--- src/components/Users/details.jsx
+++ src/components/Users/details.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import axios from "axios";
 export default function details() {
     const navigate = useNavigate();
@@ -12,6 +13,7 @@ export default function details() {
         const { data } = await axios.get(`${import.meta.env.VITE_BURL}/users/${id}`);
         setValue("userName", data.user.userName);
         setValue("email", data.user.email);
+        setValue("password", data.user.password);
         setValue("phone", data.user.phone);
         
     };
@@ -61,6 +63,7 @@ export default function details() {
                 id="Password"
                 placeholder="Password"
                 {...register("password")}
+                disabled
               />
               <label htmlFor="Password">Password</label>
             </div>
@@ -71,6 +74,7 @@ export default function details() {
                 id="phone"
                 placeholder="Phone Number"
                 {...register("phone")}
+                disabled
               />
               <label htmlFor="phone">phone Number</label>
             </div>
